8d53e51c482f0cd192d89f86d805278182f36e87
Eugene Yaremenko <eugeneyr@gmail.com>
2014-10-14 11:58:55 -0700

    Initial Commit

---
+++ CPP_Playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_Playground
+//
+//  Created by Yevhen Yaremenko on 10/14/14.
+//  Copyright (c) 2014 Yevhen Yaremenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
